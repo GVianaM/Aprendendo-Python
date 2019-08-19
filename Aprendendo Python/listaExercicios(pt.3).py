d9c5361dc99bd41c9467cfca4d9614b10fe0a195
Viana <50430246+GVianaM@users.noreply.github.com>
2019-08-19 00:53:50 -0300

    Add files via upload

--- Aprendendo Python/listaExercicios(pt.3).py
+++ Aprendendo Python/listaExercicios(pt.3).py
@@ -36,6 +36,7 @@ print("Imposto de Renda: R$", ir)
 print("INSS: R$", inss)
 print("Sindicato: R$", sind)
 print("Salário Líquido: R$", salLiquido)
+print()
 
 # 16. Faça um programa para uma loja de tintas.
 # O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
@@ -45,7 +46,27 @@ print("Salário Líquido: R$", salLiquido)
 import math
 area = float(input("Qual tamanho da área, em m2, a ser pintada?"))
 qtdLata = area / 54
-precoTotal = round(math.ceil(qtdLata), 2) * 80
-print("Para pintar uma área de", area, "m2 será preciso", round(math.ceil(qtdLata), 2), "lata(s)")
+precoTotal = math.ceil(qtdLata) * 80
+print("Para pintar uma área de", area, "m2 será preciso", math.ceil(qtdLata), "lata(s)")
 print("O valor total das latas é de: R$", round(precoTotal, 2))
 print()
+
+# 17. Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
+# Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros,
+# que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00.
+# Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
+# comprar apenas latas de 18 litros;
+# comprar apenas galões de 3,6 litros;
+# misturar latas e galões, de forma que o preço seja o menor. Acrescente 10% de folga e sempre arredonde os valores para cima,
+# isto é, considere latas cheias.
+import math
+area = float(input("Qual tamanho da área, em m2, a ser pintada?"))
+qtdLata = area / 108
+qtdGalao = area / 21.6
+precoLata = math.ceil(qtdLata) * 80
+precoGalao = math.ceil(qtdGalao) * 25
+print("Apenas latas:", math.ceil(qtdLata), " ===", precoLata, "reais")
+print("Apenas galões:",math.ceil(qtdGalao), " ===", precoGalao, "reais")
+if precoLata == precoGalao:
+    metadeLata = 50 * precoLata / 100
+    metadeGalao = 50 * precoGalao / 100
